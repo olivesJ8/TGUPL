965e319ecea4e5d6b5dbf0f39c5752ed540048c1
olivesJ8 <olivejhons8@gmail.com>
2025-02-16 13:54:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
 import os
 
 class Config(object):
-  API_ID = int(os.getenv("apiid"))
+  API_ID = int(os.getenv("apiid",7322056))
   
-  API_HASH = os.getenv("apihash")
+  API_HASH = os.getenv("apihash","89074aa74ed297150b3120914946db5c")
   
-  BOT_TOKEN = os.getenv("tk")
+  BOT_TOKEN = os.getenv("tk","7853381482:AAG3fe7mz1vLR47fJ7_GYi8bWM2XcCvJ5Vo")
   
-  AUTH = os.getenv("auth")
+  AUTH = os.getenv("auth","1387186514")
   
-  OWNER =os.getenv("owner")
+  OWNER =os.getenv("owner","1387186514")
 
-  TeraExScript = os.getenv("tscript")
-
-  PW =int(os.getenv("spw"))
+  #PW =int(os.getenv("spw"))
